8a761deaf977629ce1cc97251193e629ce29a8c5
Xiao Jia shan <649788479@qq.com>
2022-08-19 16:25:24 +0800

    Create script.py

---
+++ development/script.py
@@ -0,0 +1,115 @@
+my_name = "Codecademy"
+print("Hello and welcome " + my_name + " !")
+
+# Operators
+message = "First Name"
+message += ", Sure Name"
+print(message)
+
+# Comment on a single line
+
+user = "Jdoe" # Comment after code
+
+# Arithmetic operators
+
+result = 10 + 20
+result = 40 - 30
+result = 20 * 2
+result = 16 / 4
+result = 25 % 2
+result = 5 ** 3
+
+# Plus-equal operator
+
+counter = 0
+counter += 10
+
+# This is equalent to
+
+counter = 0
+counter = counter + 10
+
+# This operator will also perform string contactenation
+
+message = "Part 1 of message"
+message += "Part 2 of message"
+
+# There are all valid variable and assignment
+
+user_name = "codey"
+user_id = 100
+verified = False
+
+# A variable can be changed after assignment
+
+points = 100
+points = 120
+
+# Modulo operation
+
+zero = 8 % 4
+nozero = 12 % 5
+
+# Exact integer number
+
+chairs = 4
+tables = 1
+broken_chairs = -2
+sofas = 0
+
+# Non-integer numbers
+
+lights = 2.5
+left_overs = 0.0
+
+# String concatenation
+
+first = "Hello"
+second = "World"
+
+result = first + second
+
+long_reslut = first + second + "!"
+
+# Floating point number
+
+pi = 3.1415926
+meal_cost = 12.98
+tip_percent = 0.20
+
+# Print function
+print("Hello World !")
+
+print(100)
+
+pi = 3.1415926
+print(pi)
+
+# Idendity
+a = "Hello"
+b = a
+
+id(a) 
+# An interger
+
+id(a) == id(b)
+# True
+
+b = 'Hell' + 'o'
+
+id(a) == id(b)
+# True
+
+# Type
+type(a)
+
+# Type conversion
+x = '145'
+y = int(x)
+
+type(x)
+# Str
+
+type(y)
+# Int
+
